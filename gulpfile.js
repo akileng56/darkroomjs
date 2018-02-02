aa87ee43f5cdf9918e2bd1d7e8e6dca1fc47b3f4
McDoyen <mrpoloh@yahoo.co.uk>
2018-02-02 10:26:19 +0300

    Change default color to black

--- gulpfile.js
+++ gulpfile.js
@@ -81,10 +81,10 @@ gulp.task('scripts', function () {
     srcDir + '/js/plugins/darkroom.history.js',
     srcDir + '/js/plugins/darkroom.rotate.js',
     srcDir + '/js/plugins/darkroom.crop.js',
-    srcDir + '/js/plugins/darkroom.save.js',
     srcDir + '/js/plugins/darkroom.pencil.js',
     srcDir + '/js/plugins/darkroom.rectangle.js',
-    srcDir + '/js/plugins/zoom.js'
+    srcDir + '/js/plugins/zoom.js',
+    srcDir + '/js/plugins/darkroom.save.js'
   ];
 
   gulp.src(files)
